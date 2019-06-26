4a9be83399281d9ee7fd69ef2666b80ae7f4a67f
Jorge Luis Beroisa <jberoisa@gmail.com>
2019-06-26 19:48:50 -0300

    added resume

---
+++ public/resume/precache-manifest.214380904cab448f3f1e53eebc3e1b6d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "4b206ff6de7fe9fab2a6",
+    "url": "/static/js/main.4b206ff6.chunk.js"
+  },
+  {
+    "revision": "2793308452c888ac1d6b",
+    "url": "/static/js/1.27933084.chunk.js"
+  },
+  {
+    "revision": "4b206ff6de7fe9fab2a6",
+    "url": "/static/css/main.effe80eb.chunk.css"
+  },
+  {
+    "revision": "967e481777dccb88ab9c2b674f89fcdd",
+    "url": "/index.html"
+  }
+];
