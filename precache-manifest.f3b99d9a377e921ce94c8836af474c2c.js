220cac1bddf3aa62bf42f083ad65756340c7a02f
Jorge Luis Beroisa <jberoisa@gmail.com>
2019-06-26 19:58:07 -0300

    Updates

---
+++ precache-manifest.f3b99d9a377e921ce94c8836af474c2c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ca3c08a39ecc2e21869ddacb98ce3080",
+    "url": "/index.html"
+  },
+  {
+    "revision": "295718eb12e487eac81f",
+    "url": "/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "2315f74ce3c99316133d",
+    "url": "/static/js/2.3310f33a.chunk.js"
+  },
+  {
+    "revision": "295718eb12e487eac81f",
+    "url": "/static/js/main.24f001d0.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
